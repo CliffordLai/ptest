41f79db918e34790315e42b251acfb5a1623c388
CliffordLai <peakbreak@hotmail.com>
2015-06-24 00:36:12 -0400

    aa

--- R/GetFitHRegR.R
+++
@@ -1,9 +0,0 @@
-`GetFitHRegR` <-
-function (y, t = 1:length(y)) 
-{
-    theta <- numeric(2)
-    theta[1] <- length(y)
-    ans <- .C("GetHRegR", y = as.double(y), t = as.double(t), 
-        theta = as.double(theta), PACKAGE = "ptest")
-    ans$theta
-}
